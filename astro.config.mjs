1892eb6f7e745b4f526491c05f70987408bec0b7
Wannes Gennar <wannes.gennar@gmail.com>
2025-11-19 21:50:37 +0100

    feat: load Clarity directly

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,11 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
-import partytown from '@astrojs/partytown';
-
 // https://astro.build/config
 export default defineConfig({
     site: 'https://meshum.dev',
     integrations: [
 		sitemap(),
-		partytown({
-			config: {
-				forward: ['clarity']
-			}
-		})
 	],
     vite: {
         plugins: [tailwindcss()]
