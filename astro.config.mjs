3990e0663a977f4446f470757b81e67536fe84f1
Wannes Gennar <wannes.gennar+github@gmail.com>
2025-12-18 17:41:50 +0100

    feat: first pass of optimizations and style adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://meshum.dev',
+	prefetch: true,
     integrations: [
 		sitemap(),
 	],
